2bf9866a3f39573978a187506f09c518a8588708
sidhu9676 <siddasidharth2001@gmail.com>
2026-09-18 11:32:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Employee Management Application</h1>
+    <h2>Welcome to DevOps + Kubernetes!</h2>
+    <p>Application Version: 1.0</p>
+    <p>Environment: Kubernetes</p>
+    """
+
+@app.route("/health")
+def health():
+    return "Application is Healthy"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
